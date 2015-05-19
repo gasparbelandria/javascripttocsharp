285166fc5728404cf2ef77c7ce17465bfedbf0aa
Gaspar Belandria <belandria.gaspar@gmail.com>
2015-05-19 08:54:48 -0400

    Form1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -16,7 +16,7 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
-                errr
+                er
             }
             base.Dispose(disposing);
         }
