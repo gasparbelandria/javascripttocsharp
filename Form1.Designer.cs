3cbce9189fda384c94a10ffac615774e166fb898
Gaspar Belandria <belandria.gaspar@gmail.com>
2015-05-19 12:42:31 -0400

    testing CI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -16,7 +16,7 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
-                err
+                error
             }
             base.Dispose(disposing);
         }
