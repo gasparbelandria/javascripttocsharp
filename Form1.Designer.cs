9407afbc2dbf9b3c68694b9b3706b68db9b63e06
Gaspar Belandria <belandria.gaspar@gmail.com>
2015-05-27 16:55:37 -0400

    git polling

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -16,7 +16,7 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
-                // testing
+                // testing---
             }
             base.Dispose(disposing);
         }
