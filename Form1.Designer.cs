b99951f9d199db331977fdd9c15cf51e866b5139
Gaspar Belandria <belandria.gaspar@gmail.com>
2015-05-27 13:21:56 -0400

    without error - testing jenkins

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -16,7 +16,7 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
-            	error-test
+            	//error-test
             }
             base.Dispose(disposing);
         }
