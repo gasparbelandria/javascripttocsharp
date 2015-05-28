30dfb170e4c35aa4ca93b1cd054f7cc0aa01eaa7
Gaspar Belandria <belandria.gaspar@gmail.com>
2015-05-27 21:45:01 -0400

    post-commit test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -16,7 +16,7 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
-                // comments ........ comments
+                // comments ..
             }
             base.Dispose(disposing);
         }
