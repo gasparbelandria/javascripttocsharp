092155bc45338df2d764158f7bed2f1c2b8c9719
Gaspar Belandria <belandria.gaspar@gmail.com>
2015-05-28 09:09:01 -0400

    changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -16,7 +16,7 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
-                // comments
+                // CHANGES
             }
             base.Dispose(disposing);
         }
